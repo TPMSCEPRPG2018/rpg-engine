8bfa9ce4101042399346147df78f22985504ed7a
Solly <solly.ucko@gmail.com>
2018-06-02 14:44:04 -0400

    Added `TalkCharacter`s
    Signed-off-by: Solly <solly.ucko@gmail.com>

--- pygame_plugin_system/rpg.py
+++ pygame_plugin_system/rpg.py
@@ -1,6 +1,7 @@
 from enum import Enum, auto
 from operator import itemgetter
 from random import choice, random
+from time import sleep
 from typing import Dict, Optional, Tuple
 
 import pygame
@@ -34,6 +35,8 @@ class RPGWindow(QuitableWindow, ResizableWindow, TMXWindow):
         self.enemy_icon = enemy_icon
         self.tile_size = tile_size
 
+        self.map_path = ''
+        self.sprite_layer_stack = {}
         self.sprites = tmx.SpriteLayer()
         self.start_cell = (0, 0)
         super().__init__(enemy_icon=enemy_icon, tile_size=tile_size, enemy_hp=enemy_hp, player_hp=player_hp,
@@ -41,29 +44,43 @@ class RPGWindow(QuitableWindow, ResizableWindow, TMXWindow):
         self.player = Player(self.sprites, images={key: icon.convert_alpha() for key, icon in player_icons.items()},
                              window=self, location=self.start_cell, hp=player_hp)
 
+        self.init_talk_characters()
+    
     def set_tilemap(self, *, map_path: str, start_cell: Optional[Tuple[int, int]] = None, **kwargs):
+        self.sprite_layer_stack[self.map_path] = self.sprites
+    
+        self.map_path = map_path
+    
+        try:
+            self.sprites = self.sprite_layer_stack[map_path]
+        except KeyError:
+            self.sprites = tmx.SpriteLayer()
+    
+        try:
+            player = self.player
+        except AttributeError:
+            pass
+        else:
+            self.sprites.add(player)
+        
         super().set_tilemap(map_path=map_path)
-
+    
+        self.init_talk_characters()
+        
         self.tilemap.layers.append(self.sprites)
-
+    
         if start_cell is None:
             start_cell = choice(self.tilemap.layers['triggers'].match(isStart='true'))
             start_cell = start_cell.px, start_cell.py
-
+    
         self.tilemap.set_focus(*start_cell, force=True)
-
+    
         try:
             player = self.player
         except AttributeError:
             self.start_cell = start_cell
         else:
             player.rect.x, player.rect.y = start_cell
-
-    def main_loop(self):
-        super().main_loop()
-
-    def on_event(self, event: pygame.event.EventType):
-        super().on_event(event)
 
     def spawn(self, enemy: 'Enemy'):
         self.sprites.add(enemy)
@@ -72,25 +89,39 @@ class RPGWindow(QuitableWindow, ResizableWindow, TMXWindow):
         size = min(width * 100 / pygame.font.Font(None, 100).size(text)[0], height)
         return pygame.font.Font(None, int(size)).render(text, True, self.font_color)
 
-    def show_big_text(self, text: str):
+    def display_text(self, text):
         self.screen.blit(self.render_text(text, self.screen.get_rect().width, self.screen.get_rect().height), (0, 0))
+    
+    def show_big_text(self, text: str):
+        self.screen.fill(self.background)
+    
+        for obj in self.objects:
+            obj.draw(self.screen)
+    
+        self.display_text(text)
         pygame.display.flip()
-
+    
         while not pygame.key.get_pressed()[pygame.K_RETURN]:
             for event in iter(pygame.event.poll, pygame.event.Event(pygame.NOEVENT)):
                 self.on_event(event)
+    
+        sleep(.1)
+        pygame.event.pump()
 
-    def collide_rects(self, rect1, rect2):
-        if rect1.right < rect2.left:
+    @staticmethod
+    def collide_rects(rect1, rect2):
+        # Don't use pygame.Rect.collide_rect because of the *edge* case handling
+    
+        if rect1.right <= rect2.left:
             return False
     
-        if rect2.right < rect1.left:
+        if rect2.right <= rect1.left:
             return False
     
-        if rect1.bottom < rect2.top:
+        if rect1.bottom <= rect2.top:
             return False
     
-        if rect2.bottom < rect1.top:
+        if rect2.bottom <= rect1.top:
             return False
     
         return True
@@ -98,32 +129,45 @@ class RPGWindow(QuitableWindow, ResizableWindow, TMXWindow):
     def collide_border(self, location):
         window_rect = pygame.Rect(self.tilemap.view_x, self.tilemap.view_y, self.tilemap.px_width,
                                   self.tilemap.px_height)
-        if window_rect.right < location.right:
-            return False
-        if window_rect.left > location.left:
-            return False
-        if window_rect.bottom < location.bottom:
-            return False
-        if window_rect.top > location.top:
-            return False
-        return True
 
-    def collide_sprite(self, sprite1):
+        if window_rect.right < location.right:
+            return True
+        if window_rect.left > location.left:
+            return True
+        if window_rect.bottom < location.bottom:
+            return True
+        if window_rect.top > location.top:
+            return True
+
+        return False
+
+    def collide_sprite(self, location: pygame.Rect):
         collisions = set()
 
         for sprite2 in self.sprites:
-            if sprite2 is not sprite1:
-                if self.collide_rects(sprite1.rect, sprite2.rect):
+            if sprite2.rect is not location:
+                if self.collide_rects(location, sprite2.rect):
                     collisions.add(sprite2)
         
         return collisions
 
-    def can_move(self, location: pygame.Rect):
+    def collide_objects(self, location: pygame.Rect):
         for obj in self.tilemap.layers['triggers'].collide(location, 'isSolid'):
             if self.collide_rects(location, obj):
-                return False
+                return True
     
-        return self.collide_border(location)
+        return False
+    
+    def can_move(self, location: pygame.Rect):
+        return not self.collide_objects(location) and not self.collide_border(location)
+
+    def init_talk_characters(self):
+        for talk_character in self.tilemap.layers['people'].find('icon'):
+            image = pygame.image.load(talk_character['icon'])
+            image = image.convert_alpha()
+            image = pygame.transform.smoothscale(image, (self.tile_size, self.tile_size))
+            self.sprites.add(
+                TalkCharacter(window=self, image=image, location=(talk_character.top, talk_character.left)))
 
 
 class Entity(pygame.sprite.Sprite):
@@ -178,11 +222,11 @@ class Player(Entity):
             self.animate_walk()
             new.x += self.rect.width
 
-        if self.window.can_move(new):
+        if not self.window.collide_objects(new) and not self.window.collide_border(new):
             self.rect = new
 
         if old != new:
-            for enemy in self.window.collide_sprite(self):
+            for enemy in self.window.collide_sprite(self.rect):
                 self.rect = old
                 enemy.attack(1)
     
@@ -244,16 +288,6 @@ class Enemy(Entity):
         else:
             self.show_hp()
 
-    def get_player_distances(self):
-        player_rect = self.window.player.rect
-        print(self.rect, player_rect)
-        return {
-            Direction.LEFT:  (self.rect.left - player_rect.right) // self.rect.width,
-            Direction.RIGHT: (player_rect.left - self.rect.right) // self.rect.width,
-            Direction.UP:    (self.rect.top - player_rect.bottom) // self.rect.height,
-            Direction.DOWN:  (player_rect.top - self.rect.bottom) // self.rect.height
-        }
-
     def move(self, direction: Direction, distance: int = 1):
         rect = self.rect.copy()
 
@@ -269,19 +303,53 @@ class Enemy(Entity):
         return rect
 
     def tick(self):
-        player_distances = self.get_player_distances()
-        print(player_distances)
-
-        if max(player_distances.values()) <= 0:
+        if is_adjacent(self.window, self.rect):
             self.window.player.attack(1)
         else:
             try:
                 self.rect = self.move(
                     max(
-                        ((direction, distance) for direction, distance in player_distances.items() if
-                         self.window.can_move(self.move(direction)) and not self.window.collide_sprite(self)),
+                        (
+                            (direction, distance)
+                            for direction, distance
+                            in get_distances(self.window.player.rect, self.rect).items()
+                            if not self.window.collide_objects(self.move(direction)) and not self.window.collide_border(
+                            self.move(direction)) and not self.window.collide_sprite(self.move(direction))
+                        ),
                         key=itemgetter(1)
                     )[0]
                 )
             except ValueError:
                 pass  # If it's impossible to move, don't move.
+
+
+class TalkCharacter(pygame.sprite.Sprite):
+    def __init__(self, *groups, location: Tuple[int, int], window: RPGWindow, image: pygame.Surface):
+        super().__init__(*groups)
+        self.image = image
+        self.rect = pygame.rect.Rect(location, self.image.get_size())
+        self.window = window
+    
+    def attack(self, damage: float):
+        self.window.show_big_text('Hi!')
+        self.window.show_big_text('Hello!')
+    
+    def tick(self):
+        pass
+
+
+def get_player_distances(window, rect):
+    return get_distances(window.player.rect, rect)
+
+
+def get_distances(rect_1, rect_2):
+    return {
+        Direction.LEFT:  (rect_2.left - rect_1.right) // rect_2.width,
+        Direction.RIGHT: (rect_1.left - rect_2.right) // rect_2.width,
+        Direction.UP:    (rect_2.top - rect_1.bottom) // rect_2.height,
+        Direction.DOWN:  (rect_1.top - rect_2.bottom) // rect_2.height
+    }
+
+
+def is_adjacent(window, rect):
+    return all(distance <= 0 for distance in get_distances(window.player.rect, rect).values())
